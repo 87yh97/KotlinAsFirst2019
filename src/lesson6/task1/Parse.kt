24121bfa8ba95460557aac839d70dd0fe15dd5b0
87yh97 <yakubets.ns@edu.spbstu.ru>
2019-11-30 10:24:59 +0300

    .

--- src/lesson6/task1/Parse.kt
+++ src/lesson6/task1/Parse.kt
@@ -335,7 +335,7 @@ fun bestHighJump(jumps: String): Int {
 fun plusMinus(expression: String): Int {
     println()
     println("-----------START---------------")
-    if (expression == "") throw IllegalArgumentException()
+    //if (expression == "") throw IllegalArgumentException()
     val numbersAndSymbols = expression.split(" ")
     var result: Int
     val firstElement: String
