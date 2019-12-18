214cc840601fb20a4f507d684e6c493c97bfa71c
87yh97 <yakubets.ns@edu.spbstu.ru>
2019-12-18 21:53:27 +0300

    .

--- test/lesson8/task1/Tests.kt
+++ test/lesson8/task1/Tests.kt
@@ -747,6 +747,7 @@ class Tests {
         assertApproxEquals(Line(Point(0.0, 0.0), PI / 2), lineBySegment(Segment(Point(0.0, 0.0), Point(0.0, 8.0))))
         assertApproxEquals(Line(Point(1.0, 1.0), PI / 4), lineBySegment(Segment(Point(1.0, 1.0), Point(3.0, 3.0))))
         assertApproxEquals(Line(Point(0.0, 0.0), 3 * PI / 4), lineBySegment(Segment(Point(0.0, 0.0), Point(-3.0, 3.0))))
+        assertApproxEquals(Line(Point(-632.0, 5e-324), 0.0), lineBySegment(Segment(Point(-632.0, 5e-324), Point(0.7641960674013129, -2.220446049250313e-16))))
     }
 
     @Test
